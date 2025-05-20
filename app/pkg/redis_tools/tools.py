d6a8a9ec5b745d3a94e16b3b69eb63d8767a6b19
Ivan <hubble.im@mail.ru>
2025-05-20 23:21:14 +0300

    initial commit

--- app/pkg/redis_tools/tools.py
+++ app/pkg/redis_tools/tools.py
@@ -3,3 +3,11 @@ import redis
 
 class RedisTools:
     __redis_connect = redis.Redis(host='localhost', port=6379)
+
+    @classmethod
+    def set_pair(cls, pair:str, price: str):
+        cls.__redis_connect.set(pair, price)
+
+    @classmethod
+    def get_pair(cls, pair):
+        return cls.__redis_connect.get(pair)
